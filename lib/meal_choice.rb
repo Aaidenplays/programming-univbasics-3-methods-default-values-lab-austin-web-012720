ecaaae42e6055209e49b904d5ca20ab144525db8
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2019-12-27 22:06:58 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice (veg1, veg2, protein = "meat")
-  puts "enter 2 veggies and a protein"
+  puts "enter a protein"
   protein = gets
   puts "What a nutritious meal!"
   ans =  "A plate of #{protein} with #{veg1} and #{veg2}."
